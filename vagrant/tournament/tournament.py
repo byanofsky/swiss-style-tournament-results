6ddf9f33df6aa3c2ea9b4cd2adb8b1835ceb1194
byanofsky <byanofsky@me.com>
2017-01-23 20:50:57 -0800

    Change functions to underscore and setup first 3 functions

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -10,28 +10,77 @@ def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
 
-
-def deleteMatches():
+def delete_matches():
     """Remove all the match records from the database."""
+    conn = connect()
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    # Execute a command: this creates a new table
+    cur.execute("DELETE FROM matches;")
+
+    # Make the changes to the database persistent
+    conn.commit()
+
+    # Close communication with the database
+    cur.close()
+    conn.close()
 
 
-def deletePlayers():
+def delete_players():
     """Remove all the player records from the database."""
+    conn = connect()
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
 
+    # Execute a command: this creates a new table
+    cur.execute("DELETE FROM players;")
 
-def countPlayers():
+    # Make the changes to the database persistent
+    conn.commit()
+
+    # Close communication with the database
+    cur.close()
+    conn.close()
+
+def count_players():
     """Returns the number of players currently registered."""
+    conn = connect()
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    # Query the database and obtain data as Python objects
+    cur.execute("SELECT count(*) FROM players;")
+    return cur.fetchone()
+
+    # Close communication with the database
+    cur.close()
+    conn.close()
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    # Pass data to fill a query placeholders and let Psycopg perform
+    # the correct conversion (no more SQL injections!)
+    cur.execute("INSERT INTO players (name) VALUES (%s)", (name,))
+
+    # Make the changes to the database persistent
+    conn.commit()
+
+    # Close communication with the database
+    cur.close()
+    conn.close()
 
 
 def playerStandings():
@@ -56,16 +105,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +122,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
