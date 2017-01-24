2a37275383897cd44aa48e61daab8306cecdbf5a
byanofsky <byanofsky@me.com>
2017-01-23 20:52:36 -0800

    Change function names in test file

--- vagrant/tournament/tournament_test.py
+++ vagrant/tournament/tournament_test.py
@@ -14,8 +14,8 @@ def testCount():
              player count after 1 and 2 players registered,
              player count after players deleted.
     """
-    deleteMatches()
-    deletePlayers()
+    delete_matches()
+    delete_players()
     c = countPlayers()
     if c == '0':
         raise TypeError(
@@ -23,20 +23,20 @@ def testCount():
     if c != 0:
         raise ValueError("After deletion, countPlayers should return zero.")
     print "1. countPlayers() returns 0 after initial deletePlayers() execution."
-    registerPlayer("Chandra Nalaar")
-    c = countPlayers()
+    register_player("Chandra Nalaar")
+    c = count_players()
     if c != 1:
         raise ValueError(
             "After one player registers, countPlayers() should be 1. Got {c}".format(c=c))
     print "2. countPlayers() returns 1 after one player is registered."
-    registerPlayer("Jace Beleren")
-    c = countPlayers()
+    register_player("Jace Beleren")
+    c = count_players()
     if c != 2:
         raise ValueError(
             "After two players register, countPlayers() should be 2. Got {c}".format(c=c))
     print "3. countPlayers() returns 2 after two players are registered."
-    deletePlayers()
-    c = countPlayers()
+    delete_players()
+    c = count_players()
     if c != 0:
         raise ValueError(
             "After deletion, countPlayers should return zero.")
