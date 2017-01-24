0fc07bc94d8de782bf828fdab00ddbf59a40e582
byanofsky <byanofsky@me.com>
2017-01-24 08:55:56 -0800

    Create matches and winners tables

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -9,5 +9,18 @@
 DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 \c tournament
-CREATE TABLE players (id serial PRIMARY KEY, name text);
-CREATE TABLE matches (id serial PRIMARY KEY, p1id integer, p2id integer);
+CREATE TABLE players (
+    id serial PRIMARY KEY,
+    name text
+);
+CREATE TABLE matches (
+    id serial,
+    pid integer REFERENCES players (id),
+    PRIMARY KEY (id, pid)
+);
+CREATE TABLE winners (
+    match_id integer,
+    pid integer REFERENCES players (id),
+    PRIMARY KEY (match_id, pid),
+    FOREIGN KEY (match_id, pid) REFERENCES matches (id, pid)
+);
