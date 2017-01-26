00ea7fc510e199382399cbe0176b64018f6b78d4
byanofsky <byanofsky@me.com>
2017-01-26 13:10:15 -0800

    Add a score view

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -25,7 +25,7 @@ CREATE TABLE matches (
 
 CREATE VIEW win_counts_v AS
     SELECT players.id,
-        count(matches.id) as win_count
+        count(matches.id) AS win_count
     FROM players
     LEFT JOIN matches
     ON players.id = matches.winner
@@ -34,10 +34,22 @@ CREATE VIEW win_counts_v AS
 
 CREATE VIEW match_counts_v AS
     SELECT players.id,
-        count(matches.id) as match_count
+        count(matches.id) AS match_count
     FROM players
     LEFT JOIN matches
     ON players.id = matches.winner
         OR players.id = matches.loser
     GROUP BY players.id
     ORDER BY players.id;
+
+CREATE VIEW score_v AS
+    SELECT players.id,
+           players.name,
+           COALESCE(sum(wins.win_count), 0) AS score
+    FROM players
+    LEFT JOIN matches
+        ON players.id = matches.winner
+    LEFT JOIN win_counts_v AS wins
+        ON wins.id = matches.loser
+    GROUP BY players.id
+    ORDER BY score DESC;
