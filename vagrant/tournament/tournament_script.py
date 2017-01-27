09e65566fb0437e12a72248fa27d41669e40c02d
byanofsky <byanofsky@me.com>
2017-01-26 18:06:44 -0800

    Remove play functions from script

--- vagrant/tournament/tournament_script.py
+++ vagrant/tournament/tournament_script.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+#
+# Helper scripts for running tournament.
+#
+
 from tournament import *
 
 def register_8_players():
@@ -22,56 +27,12 @@ def random_matches(n):
         report_match(players[p1][0], players[p2][0])
     print "Matches reported: %s" % count_matches()
 
-def play_match(pair):
-    """
-    Determines winner and loser of match, based on 50/50 probability.
-
-    Args:
-      pair: tuple representing player pair (id1, name1, id2, name2)
-
-    Returns:
-      A tuple containing 2 tuples "(winner, loser)", one for winner and one for loser, each
-      containing:
-        id: player's id
-        name: player's name
-    """
-    # Determine winner and loser positions (0 or 1)
-    w = random.randint(0,1)
-    l = (w+1) % 2
-    ids = (pair[0], pair[2])
-    names = (pair[1], pair[3])
-    print "%s beats %s" % (names[w], names[l])
-    return ((ids[w], names[w]), (ids[l], names[l]))
-
-
-
-# def play():
-#     """
-#     Play a tournament
-#     """
-#     num_players = count_players()
-#     rounds = math.log(num_players, 2)
-#     for r in range(rounds):
-#         standings = player_standings()
-#         [id1, id2, id3, id4] = [row[0] for row in standings]
-#         if random.randint(0,1):
-#             report_match(id1, id2)
-#         else:
-#             report_match(id2, id1)
-#         if random.randint(0,1):
-#             report_match(id3, id4)
-#         else:
-#             report_match(id4, id3)
-#     print player_standings()
-
 
 if __name__ == '__main__':
     delete_matches()
     delete_players()
     register_8_players()
-    play_one_round(play_match)
+    standings = play_tournament()
     print "\nMatch played. Player standings:"
-    standings = player_standings()
     for player in standings:
         print "    %s, wins: %s" % (player[1], player[2])
-    # random_matches(8)
