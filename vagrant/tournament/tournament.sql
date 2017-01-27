158ba5443772f4218f301b4603eb18dbe8c64275
byanofsky <byanofsky@me.com>
2017-01-26 18:17:55 -0800

    Fix sql to match style guide

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -13,42 +13,41 @@ CREATE DATABASE tournament;
 \c tournament
 
 CREATE TABLE players (
-    id serial PRIMARY KEY,
-    name text
+  id serial PRIMARY KEY,
+  name text
 );
 
 CREATE TABLE matches (
-    id serial PRIMARY KEY,
-    winner integer REFERENCES players (id),
-    loser integer REFERENCES players (id)
+  id serial PRIMARY KEY,
+  winner integer REFERENCES players (id),
+  loser integer REFERENCES players (id)
 );
 
 -- A view for count of wins for each player
 CREATE VIEW win_counts_v AS
-    SELECT players.id,
-        count(matches.id) AS win_count
-    FROM players
-    LEFT JOIN matches
-    ON players.id = matches.winner
-    GROUP BY players.id;
+  SELECT
+    players.id,
+    count(matches.id) AS win_count
+  FROM players
+  LEFT JOIN matches ON players.id = matches.winner
+  GROUP BY players.id;
 
 -- A view for count of matches for each player
 CREATE VIEW match_counts_v AS
-    SELECT players.id,
-        count(matches.id) AS match_count
-    FROM players
-    LEFT JOIN matches
-    ON players.id = matches.winner
-        OR players.id = matches.loser
-    GROUP BY players.id;
+  SELECT
+    players.id,
+    count(matches.id) AS match_count
+  FROM players
+  LEFT JOIN matches
+  ON players.id = matches.winner OR players.id = matches.loser
+  GROUP BY players.id;
 
 -- A view for each player's score (sum of beaten opponents' scores)
 CREATE VIEW score_v AS
-    SELECT players.id,
-           COALESCE(sum(wins.win_count), 0) AS score
-    FROM players
-    LEFT JOIN matches
-        ON players.id = matches.winner
-    LEFT JOIN win_counts_v AS wins
-        ON wins.id = matches.loser
-    GROUP BY players.id;
+  SELECT
+    players.id,
+    COALESCE(sum(wins.win_count), 0) AS score
+  FROM players
+  LEFT JOIN matches ON players.id = matches.winner
+  LEFT JOIN win_counts_v AS wins ON wins.id = matches.loser
+  GROUP BY players.id;
